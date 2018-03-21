0a5a4da0bf1ce04f3632996d8c28fa1f2a577ec1
Dmitriy Parkhomenko <parkhomenko@Dmitriys-MacBook-Pro.local>
2018-03-21 10:23:50 +0200

    initial commit

---
+++ unit1/app.js
@@ -0,0 +1,13 @@
+require('./instantHello');
+var goodbye = require('./talk/goodbye');
+var talk = require('./talk');
+var question = require('./talk/question');
+
+talk.intro();
+talk.hello("Simon");
+
+var answer = question.ask("What is the meaning of life?");
+
+console.log(answer);
+
+goodbye();
